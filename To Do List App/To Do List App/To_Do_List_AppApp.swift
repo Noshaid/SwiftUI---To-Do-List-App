662806cd2b4fe23912b08eb44dd47a0a58c18856
Noshaid Ali <noshaid.ali@coeus-solutions.de>
2024-08-11 11:58:25 +0500

    Project created

---
+++ To Do List App/To Do List App/To_Do_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_AppApp.swift
+//  To Do List App
+//
+//  Created by Noshaid Ali on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
